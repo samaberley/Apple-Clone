bc23d61c5cc6d1352da1af4bfce974a88f128c2a
Sam <samaberley@gmail.com>
2024-08-07 10:21:46 +1000

    Fixed spelling mistake screen max width

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { appleImg } from '../utils';
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <img src={appleImg} alt= "Apple" width={14} height={18} />
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
